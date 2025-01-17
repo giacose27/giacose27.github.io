8af66a5b8b5426fe915f617d90d080109a482691
giacose27 <gianna.cosenza@students.first.edu>
2025-01-17 12:56:23 -0500

    Added Styles

--- gia-sandbox/js/script.js
+++ gia-sandbox/js/script.js
@@ -1,15 +1,5 @@
-const htmlBody = document.querySelector('body');
-htmlBody
-    
-const randomClickFunction = function () {
-    const colors = ["#002942", "0Ca7DB", "red", "blue", "green"]
-
-    const randomIndex = Math.floor(Math.random() * colors.length);
-
-    const randomColor = colors[randomIndex];
-
-    htmlBody.style.backgroundColor = randomColor;
-
-    console.log('The user clicked and se the color to' + randomColor);
-}
-randomClickFunction()
+<script>
+    function onClickEvent(){
+        
+    }
+</script>
